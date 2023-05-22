719bbf426a4b7080a7cadde6c6a8e82c90e9e296
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-05-22 02:13:04 -0300

    feat(nuget): update package

---
+++ src/packages/csharp/EZlion/EZlion/Mapper/WeaponClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZlion.Mapper
+{
+    ///<summary>
+    ///The weapon class list
+    ///</summary>
+    public static class WeaponClass
+    {
+        /// <summary>
+        /// Gets the name of the identifier.
+        /// </summary>
+        /// <value>
+        /// The name of the identifier.
+        /// </value>
+        public static IReadOnlyDictionary<int, string> IDName { get; } = new Dictionary<int, string>
+        {
+            {0, "Blademaster"},
+            {1, "Gunner"},
+            {2, "Blademaster"},
+            {3, "Blademaster"},
+            {4, "Blademaster"},
+            {5, "Gunner"},
+            {6, "Blademaster"},
+            {7, "Blademaster"},
+            {8, "Blademaster"},
+            {9, "Blademaster"},
+            {10, "Gunner"},
+            {11, "Blademaster"},
+            {12, "Blademaster"},
+            {13, "Blademaster"}
+        };
+    };
+}
---
+++ src/packages/csharp/EZlion/EZlion/Mapper/WeaponStyle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZlion.Mapper
+{
+    ///<summary>
+    ///The weapon styles list
+    ///</summary>
+    public static class WeaponStyle
+    {
+        /// <summary>
+        /// Gets the name of the identifier.
+        /// </summary>
+        /// <value>
+        /// The name of the identifier.
+        /// </value>
+        public static IReadOnlyDictionary<int, string> IDName { get; } = new Dictionary<int, string>
+        {
+            {0, "Earth Style"},
+            {1, "Heaven Style"},
+            {2, "Storm Style"},
+            {3, "Extreme Style"}
+        };
+    };
+}
