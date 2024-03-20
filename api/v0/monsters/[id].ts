adb64c0c3a179511fc5f9ba6d16f140ca8b3b23f
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-03-20 08:34:10 -0300

    feat: update api

--- api/v0/monsters/[id].ts
+++ api/v0/monsters/[id].ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default (req: VercelRequest, res: VercelResponse) => {
+export function GET(req: VercelRequest, res: VercelResponse) {
   try {
     if (req.method !== "GET") {
       return res.status(405).json({ error: "Method Not Allowed" });
@@ -10,10 +10,10 @@ export default (req: VercelRequest, res: VercelResponse) => {
 
     const { id } = req.query;
     if (typeof id !== "string") {
-      return res.status(404).json({ error: `Monster id ${id} not found` });
+      return res.status(400).json({ error: `Bad request.` });
     }
 
-    const dataPath = join(__dirname, "data.json");
+    const dataPath = join(process.cwd(), "api", "v0", "monsters", "data.json");
     const data = JSON.parse(readFileSync(dataPath, "utf8"));
     const monster = data.results.find(
       (m: { id: number }) => m.id === parseInt(id, 10)
@@ -27,4 +27,4 @@ export default (req: VercelRequest, res: VercelResponse) => {
   } catch (err) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
-};
+}
--- api/v0/monsters/index.ts
+++ api/v0/monsters/index.ts
@@ -2,16 +2,16 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default (req: VercelRequest, res: VercelResponse) => {
+export function GET(req: VercelRequest, res: VercelResponse) {
   try {
     if (req.method !== "GET") {
       return res.status(405).json({ error: "Method Not Allowed" });
     }
 
-    const dataPath = join(__dirname, "data.json");
+    const dataPath = join(process.cwd(), "api", "v0", "monsters", "data.json");
     const data = JSON.parse(readFileSync(dataPath, "utf8"));
     return res.status(200).json(data.results);
   } catch (err) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
-};
+}
