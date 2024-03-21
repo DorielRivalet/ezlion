c2cf151d16a30a5843e34df4a015a0affd7ca04a
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-03-21 11:13:31 -0300

    feat: update swagger

---
+++ api/docs/json/index.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  try {
+    if (req.method !== "GET") {
+      return res
+        .status(405)
+        .setHeader("Content-Type", "application/problem+json")
+        .json({
+          type: "https://httpstatuses.com/405",
+          title: "Method Not Allowed",
+          status: 405,
+          detail:
+            "The method specified in the Request-Line is not allowed for the resource identified by the Request-URI. The Ezlion API is read-only.",
+        });
+    }
+
+    const dataPath = join(process.cwd(), "api", "docs", "json", "swagger.json");
+    const data = JSON.parse(readFileSync(dataPath, "utf8"));
+    return res.status(200).json(data.results);
+  } catch (err) {
+    return res
+      .status(500)
+      .setHeader("Content-Type", "application/problem+json")
+      .json({
+        type: "https://httpstatuses.com/500",
+        title: "Internal Server Error",
+        status: 500,
+        detail:
+          "The server encountered an unexpected condition that prevented it from fulfilling the request. Contact the API developers for help.",
+      });
+  }
+};
