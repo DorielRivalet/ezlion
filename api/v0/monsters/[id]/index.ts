d09fe2a8c720e3ebf4b3b2188d17906144292744
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-03-21 09:15:17 -0300

    feat: update api

--- api/v0/monsters/[id]/index.ts
+++ api/v0/monsters/[id]/index.ts
@@ -5,13 +5,25 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default (req: VercelRequest, res: VercelResponse) => {
   try {
     if (req.method !== "GET") {
-      return res.status(405).json({ error: "Method Not Allowed" });
+      return res.status(405).json({
+        type: "https://httpstatuses.com/405",
+        title: "Method Not Allowed",
+        status: 405,
+        detail:
+          "The method specified in the Request-Line is not allowed for the resource identified by the Request-URI. The Ezlion API is read-only.",
+      });
     }
 
-    const url = req.url; // Assuming req.url contains the full URL path
+    const url = req.url;
 
     if (url === undefined) {
-      return res.status(400).json({ error: `Bad response.` });
+      return res.status(400).json({
+        type: "https://httpstatuses.com/400",
+        title: "Bad Request",
+        status: 400,
+        detail:
+          "The server cannot or will not process the request due to something that is perceived to be a client error. The url is undefined.",
+      });
     }
 
     const lastSlashIndex = url.lastIndexOf("/");
@@ -20,7 +32,11 @@ export default (req: VercelRequest, res: VercelResponse) => {
 
     if (Number.isNaN(id)) {
       return res.status(400).json({
-        error: `Bad response.`,
+        type: "https://httpstatuses.com/400",
+        title: "Bad Request",
+        status: 400,
+        detail:
+          "The server cannot or will not process the request due to something that is perceived to be a client error. The id is not a number.",
       });
     }
 
@@ -31,9 +47,20 @@ export default (req: VercelRequest, res: VercelResponse) => {
     if (monster) {
       return res.status(200).json(monster);
     } else {
-      return res.status(404).json({ error: `Monster id ${id} not found` });
+      return res.status(404).json({
+        type: "https://httpstatuses.com/404",
+        title: "Not Found",
+        status: 404,
+        detail: `The origin server did not find a current representation for the target resource or is not willing to disclose that one exists. Monster id not found.`,
+      });
     }
   } catch (err) {
-    return res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({
+      type: "https://httpstatuses.com/500",
+      title: "Internal Server Error",
+      status: 500,
+      detail:
+        "The server encountered an unexpected condition that prevented it from fulfilling the request. Contact the API developers for help.",
+    });
   }
 };
