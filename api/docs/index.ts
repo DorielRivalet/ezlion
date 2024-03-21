f2fc090f6c0e6ea79ae8d9f7bd1d0ab93a3ea52a
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-03-21 10:46:36 -0300

    feat: update api

--- api/docs/index.ts
+++ api/docs/index.ts
@@ -1,17 +1,34 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { SwaggerUIBundle } from "swagger-ui-dist";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  const swaggerHtml = SwaggerUIBundle({
-    url: "/api/docs/swagger.json",
-    dom_id: "#swagger-ui",
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIBundle.SwaggerUIStandalonePreset,
-    ],
-    layout: "StandaloneLayout",
-  });
+  try {
+    const ui = SwaggerUIBundle({
+      spec: JSON.parse(
+        readFileSync(join(process.cwd(), "api", "docs", "swagger.json"), "utf8")
+      ),
+      dom_id: "#swagger-ui",
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset,
+      ],
+      layout: "StandaloneLayout",
+    });
 
-  res.setHeader("Content-Type", "text/html");
-  res.send(swaggerHtml);
+    res.setHeader("Content-Type", "text/html");
+    return res.send(ui);
+  } catch {
+    return res
+      .status(500)
+      .setHeader("Content-Type", "application/problem+json")
+      .json({
+        type: "https://httpstatuses.com/500",
+        title: "Internal Server Error",
+        status: 500,
+        detail:
+          "The server encountered an unexpected condition that prevented it from fulfilling the request. Contact the API developers for help.",
+      });
+  }
 };
