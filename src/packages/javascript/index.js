936324c70217255b13d080f2fc454a56ff131591
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-08-03 00:57:53 -0300

    feat(js)!: make imports modular

--- src/packages/javascript/index.js
+++ src/packages/javascript/index.js
@@ -1,69 +1,67 @@
-import ArmorArms from "./Mapper/ArmorArms.js";
-import ArmorChest from "./Mapper/ArmorChest.js";
-import ArmorColor from "./Mapper/ArmorColor.js";
-import ArmorHead from "./Mapper/ArmorHead.js";
-import ArmorLegs from "./Mapper/ArmorLegs.js";
-import ArmorWaist from "./Mapper/ArmorWaist.js";
-import Item from "./Mapper/Item.js";
-import Location from "./Mapper/Location.js";
-import Monster from "./Mapper/Monster.js";
-import ObjectiveType from "./Mapper/ObjectiveType.js";
-import PoogieCostume from "./Mapper/PoogieCostume.js";
-import Quest from "./Mapper/Quest.js";
-import RankBand from "./Mapper/RankBand.js";
-import Sharpness from "./Mapper/Sharpness.js";
-import SkillArmor from "./Mapper/SkillArmor.js";
-import SkillArmorPriority from "./Mapper/SkillArmorPriority.js";
-import SkillCaravan from "./Mapper/SkillCaravan.js";
-import SkillDiva from "./Mapper/SkillDiva.js";
-import SkillHalk from "./Mapper/SkillHalk.js";
-import SkillRoadTower from "./Mapper/SkillRoadTower.js";
-import SkillSigil from "./Mapper/SkillSigil.js";
-import SkillStyleRank from "./Mapper/SkillStyleRank.js";
-import SkillZenith from "./Mapper/SkillZenith.js";
-import WeaponBlademaster from "./Mapper/WeaponBlademaster.js";
-import WeaponClass from "./Mapper/WeaponClass.js";
-import WeaponGunner from "./Mapper/WeaponGunner.js";
-import WeaponStyle from "./Mapper/WeaponStyle.js";
-import WeaponType from "./Mapper/WeaponType.js";
-import PoogieGuildOutfit from "./Mapper/PoogieGuildOutfit.js";
-import QuestToggleMode from "./Mapper/QuestToggleMode.js";
-import SkillDivaPrayerGem from "./Mapper/SkillDivaPrayerGem.js";
-import SkillGuildPoogie from "./Mapper/SkillGuildPoogie.js";
-import SkillTree from "./Mapper/SkillTree.js";
+import ezlionArmorArms from "./Mapper/ArmorArms.js";
+import ezlionArmorChest from "./Mapper/ArmorChest.js";
+import ezlionArmorColor from "./Mapper/ArmorColor.js";
+import ezlionArmorHead from "./Mapper/ArmorHead.js";
+import ezlionArmorLegs from "./Mapper/ArmorLegs.js";
+import ezlionArmorWaist from "./Mapper/ArmorWaist.js";
+import ezlionItem from "./Mapper/Item.js";
+import ezlionLocation from "./Mapper/Location.js";
+import ezlionMonster from "./Mapper/Monster.js";
+import ezlionObjectiveType from "./Mapper/ObjectiveType.js";
+import ezlionPoogieCostume from "./Mapper/PoogieCostume.js";
+import ezlionQuest from "./Mapper/Quest.js";
+import ezlionRankBand from "./Mapper/RankBand.js";
+import ezlionSharpness from "./Mapper/Sharpness.js";
+import ezlionSkillArmor from "./Mapper/SkillArmor.js";
+import ezlionSkillArmorPriority from "./Mapper/SkillArmorPriority.js";
+import ezlionSkillCaravan from "./Mapper/SkillCaravan.js";
+import ezlionSkillDiva from "./Mapper/SkillDiva.js";
+import ezlionSkillHalk from "./Mapper/SkillHalk.js";
+import ezlionSkillRoadTower from "./Mapper/SkillRoadTower.js";
+import ezlionSkillSigil from "./Mapper/SkillSigil.js";
+import ezlionSkillStyleRank from "./Mapper/SkillStyleRank.js";
+import ezlionSkillZenith from "./Mapper/SkillZenith.js";
+import ezlionWeaponBlademaster from "./Mapper/WeaponBlademaster.js";
+import ezlionWeaponClass from "./Mapper/WeaponClass.js";
+import ezlionWeaponGunner from "./Mapper/WeaponGunner.js";
+import ezlionWeaponStyle from "./Mapper/WeaponStyle.js";
+import ezlionWeaponType from "./Mapper/WeaponType.js";
+import ezlionPoogieGuildOutfit from "./Mapper/PoogieGuildOutfit.js";
+import ezlionQuestToggleMode from "./Mapper/QuestToggleMode.js";
+import ezlionSkillDivaPrayerGem from "./Mapper/SkillDivaPrayerGem.js";
+import ezlionSkillGuildPoogie from "./Mapper/SkillGuildPoogie.js";
+import ezlionSkillTree from "./Mapper/SkillTree.js";
 
-export default {
-  ArmorArms,
-  ArmorChest,
-  ArmorColor,
-  ArmorHead,
-  ArmorLegs,
-  ArmorWaist,
-  Item,
-  Location,
-  Monster,
-  ObjectiveType,
-  PoogieCostume,
-  Quest,
-  RankBand,
-  Sharpness,
-  SkillArmor,
-  SkillArmorPriority,
-  SkillCaravan,
-  SkillDiva,
-  SkillHalk,
-  SkillRoadTower,
-  SkillSigil,
-  SkillStyleRank,
-  SkillZenith,
-  WeaponBlademaster,
-  WeaponClass,
-  WeaponGunner,
-  WeaponStyle,
-  WeaponType,
-  PoogieGuildOutfit,
-  QuestToggleMode,
-  SkillDivaPrayerGem,
-  SkillGuildPoogie,
-  SkillTree,
-};
+export { ezlionArmorArms };
+export { ezlionArmorChest };
+export { ezlionArmorColor };
+export { ezlionArmorHead };
+export { ezlionArmorLegs };
+export { ezlionArmorWaist };
+export { ezlionItem };
+export { ezlionLocation };
+export { ezlionMonster };
+export { ezlionObjectiveType };
+export { ezlionPoogieCostume };
+export { ezlionQuest };
+export { ezlionRankBand };
+export { ezlionSharpness };
+export { ezlionSkillArmor };
+export { ezlionSkillCaravan };
+export { ezlionSkillDiva };
+export { ezlionSkillHalk };
+export { ezlionSkillRoadTower };
+export { ezlionSkillSigil };
+export { ezlionSkillStyleRank };
+export { ezlionSkillZenith };
+export { ezlionWeaponBlademaster };
+export { ezlionWeaponClass };
+export { ezlionWeaponGunner };
+export { ezlionWeaponStyle };
+export { ezlionWeaponType };
+export { ezlionPoogieGuildOutfit };
+export { ezlionQuestToggleMode };
+export { ezlionSkillDivaPrayerGem };
+export { ezlionSkillGuildPoogie };
+export { ezlionSkillTree };
+export { ezlionSkillArmorPriority };
